50b2224accce0aae04b2608b8d7975157274d4e5
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-19 16:42:50 +0100

    add elements and edit js variables

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-*/
 
-/* tools
-- const e let
-- if else
-- console.log
-- prompt per chiedere all'utente quanti km vuole percorrere
-- prompt per chiedere l'età del passeggero
-*/
 
 //PREPARAZIONE
 
@@ -38,3 +24,23 @@ if (userAge < 18) {
 
 //numero in decimali da stampare in console
 console.log(priceResult.toFixed(2));
+
+// select the form input elements
+const formEl = document.querySelector(".price")
+const nameEl = document.querySelector(".userName")
+const kmEl = document.querySelector("km")
+const usernameEl = document.querySelector("username")
+//continua
+
+console.log(formEl, nameEl.value);
+
+formEl.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // gather the data
+    // potevo mettere il valore in una costante e poi sotto mettere la costante (es. companyEl.value)
+    // update the card's values
+    cardCompany.innerHTML = companyEl.value
+    // continua
+    cardImage.src = imageEl.value
+})
