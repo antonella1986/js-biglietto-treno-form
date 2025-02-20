dbc45b50d0b7cb89a8e7d0d3212a854774bc991b
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-20 12:27:18 +0100

    correction

--- main.js
+++ main.js
@@ -1,45 +1,53 @@
-// PREPARAZIONE
-
-// raccolta dati
-/* const userChoise = Number(prompt ("Quanti km vuoi percorrere?"));
-console.log(userChoise);
-
-const userAge = Number(prompt ("Quanti anni hai?"));
-console.log(userAge); */
-
-const pricePerKm = 0.21;
-
-let priceResult = userChoise * pricePerKm;
-
-// ESECUZIONE
-
-// verifica
-if (userAge < 18) {
-    priceResult *= 0.80;
-} else if (userAge > 65) {
-    priceResult *= 0.60;
-}
-
 //numero in decimali da stampare in console
 console.log(priceResult.toFixed(2));
 
-// select the form input elements
+// select the DOM elements
 const formEl = document.querySelector(".price")
-const nameEl = document.querySelector(".userName")
 const kmEl = document.querySelector(".km")
+const ageEl = document.querySelector("age")
+const nameEl = document.querySelector("name")
+const passengerEl = document.querySelector("passenger-name")
+const offerEl = document.querySelector("offer-name")
+const wagonEl = document.querySelector("wagon")
+const cpCodeEl = document.querySelector("cp-code")
+const priceEl = document.querySelector("ticket-price")
 
-// select the card elements
-const cardCost = 
+const pricePerKm = 0.21
+const minorDiscount = 0.20
+const seniorDiscount = 0.40
+let ticketPrice
+let discountName
 
-console.log(formEl, nameEl.value, kmEl.value);
+console.log(formEl, ageEl, kmEl, pricePerKm, minorDiscount, seniorDiscount, ticketPrice);
 
+// add an event listener to the form listen for the submit element
 formEl.addEventListener("submit", function(e) {
     e.preventDefault();
-    console.log(priceResult());
     
+    // read and save km and age values from the selected inputs
+    const kmValue = kmEl.value
+    const ageValue = ageEl.value
+    const nameValue = nameEl.value
+    
+    // elaboration of the ticket price
+    ticketPrice = kmValue * pricePerKm;
+    
+    console.log(ticketPrice, kmEl, ageValue, nameValue);
 
-    // gather the data
-
-    // update the cards' values
-    cardCompany.innerHTML = companyEl.value
+    
+    // verify user age and apply discount
+    if (ageValue === "minor") {
+        console.log(20% off);
+        ticketPrice -= ticketPrice * minorDiscount
+    } else if (ageValue === "senior") {
+        console.log(40% off);
+        ticketPrice -= ticketPrice * seniorDiscount
+    } else {
+        discountName = "Standard ticket"
+    }
+            
+    // update the DOM and print ticket data
+    passengerEl.innerText = nameValue
+    offerNameEl.innerText = discountName
+    priceEl.innerText = ticketPrice
 })
